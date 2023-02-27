3401b5fabd82922f36a42b3e38a6499e975b76dc
NISAN05 <sanoranigtherson@yahoo.com>
2023-02-27 11:56:08 -0800

    code java et xml

--- app/src/main/java/com/example/flashcardproject/MainActivity.java
+++ app/src/main/java/com/example/flashcardproject/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.flashcardproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,16 +15,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ecouteVueButton();
+        ecouteReponse1();
+        ecouteReponse2();
+        ecouteReponse3();
 
-        TextView questionText = findViewById(R.id.flashcard_question_textview);
-        questionText.setOnClickListener(new View.OnClickListener() {
+    }
+
+    /**
+     * cette methode ecoute le textview1
+     */
+    private void ecouteReponse1(){
+        TextView Reponse1=findViewById(R.id.IdReponse1);
+        Reponse1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "I CLICKED THE QUESTION!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "MAUVAISE REPONSE!", Toast.LENGTH_SHORT).show();
+                Reponse1.setBackgroundColor(Color.RED);
+            }
+        });
+    }
+
+    /**
+     * cette methode ecoute le textview2
+     */
+    private void ecouteReponse2(){
+        TextView Reponse2=findViewById(R.id.IdReponse2);
+        Reponse2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView)findViewById(R.id.IdQuestion)).setText("Bonne reponse, Felicitations!");
+                Reponse2.setBackgroundColor(Color.GREEN);
+            }
+        });
+    }
+
+    /**
+     * cette methode ecoute le textview 3
+     */
+    private void ecouteReponse3(){
+        TextView Reponse3=findViewById(R.id.IdReponse3);
+        Reponse3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "MAUVAISE REPONSE!", Toast.LENGTH_SHORT).show();
+                Reponse3.setBackgroundColor(Color.RED);
+            }
+        });
+    }
+
+    /**
+     * cette methode ecoute le bouton de vue et fait apparaitre ou dispaeaitre les reponse
+     */
+    private void ecouteVueButton(){
+
+        TextView REPONSE1=findViewById(R.id.IdReponse1);
+        TextView REPONSE2=findViewById(R.id.IdReponse2);
+        TextView REPONSE3=findViewById(R.id.IdReponse3);
+
+        ImageButton ButtonVue = findViewById(R.id.IdVueButton);
+        ButtonVue.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(REPONSE1.getVisibility()==View.VISIBLE){
+                    REPONSE1.setVisibility(View.GONE);
+                    REPONSE2.setVisibility(View.GONE);
+                    REPONSE3.setVisibility(View.GONE);
+                }else {
+                    REPONSE1.setVisibility(View.VISIBLE);
+                    REPONSE2.setVisibility(View.VISIBLE);
+                    REPONSE3.setVisibility(View.VISIBLE);
+                }
+
 
             }
         });
-
 
     }
 }
